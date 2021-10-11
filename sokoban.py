46d85b2a50f94397c729fe210e30e77ccf2415e9
remsokawaii1 <remsokawaii1@gmail.com>
2021-10-11 11:07:56 +0700

    hotfix and add some input

--- sokoban.py
+++ sokoban.py
@@ -5,27 +5,30 @@ import time
 this_dir = sys.path[0]
 
 MAP = []
-ALL_STATES = []
+ALL_STATES = set()
 dx = [-1, 1, 0, 0] 
 dy = [0, 0, -1, 1] # move: up, down, left, right
 
-N, M = 0, 0 # size of map
+N, M = 0, 0 # size of map (height: N, width: M)
 pos = (0, 0) # player position
 boxs, docks, goals = [], [], {} #list of box, dock, box on dock
 
 def printInfo():
-    print("@ - palyer")
+    print("@ - player")
     print("$ - box")
     print(". - dock")
     print("+ - player on dock")
     print("* - box on dock (goal)")
     print("# - wall \n")
 
-    print("\n <Init state> \n")
+    print("\n <INIT STATE> \n")
     for m in MAP:
         print(*m)
 
-    print("\n <Searching> \n")
+    print("\nPress ENTER to start finding solution!")
+    keyboard.wait("enter")
+
+    print("\n <FINDING SOLUTION...> \n")
 
 def readMap(inPut):
     # load map data to MAP
@@ -33,6 +36,7 @@ def readMap(inPut):
     for line in f:
         if len(line) > 1:
             MAP.append(list(line.strip()))
+    f.close()
 
 def init():
     # extract all data to variable
@@ -41,12 +45,20 @@ def init():
         for j in range(M):
             if MAP[i][j] == "@":
                 pos = (i, j)
+
+            if MAP[i][j] == "+":
+                pos = (i, j)
+                docks.append((i, j))
+                goals[(i, j)] = False
+
             if MAP[i][j] == "$":
                 boxs.append((i, j))
+
             if MAP[i][j] == ".":
                 docks.append((i, j))
                 goals[(i, j)] = False
             if MAP[i][j] == "*":
+
                 boxs.append((i, j))
                 docks.append((i, j))
                 goals[(i, j)] = True
@@ -115,7 +127,7 @@ def validMove(x, y, state, direction):
     elif hs in ALL_STATES:
             return False
 
-    ALL_STATES.append(hs)
+    ALL_STATES.add(hs)
     return True
 
 def moveBox(x, y, direction, boxs, goals):
@@ -148,14 +160,14 @@ def printMap(state):
     docks = state[2]
 
     MAP2[pos[0]][pos[1]] = "@"
-    for b in boxs:
-        MAP2[b[0]][b[1]] = "$"
-    for d in docks:
-        if MAP2[d[0]][d[1]] == "$":
-            MAP2[d[0]][d[1]] = "*"
-        elif MAP2[d[0]][d[1]] == "@":
-            MAP2[d[0]][d[1]] = "+"
-        else: MAP2[d[0]][d[1]] = "."
+    for box in boxs:
+        MAP2[box[0]][box[1]] = "$"
+    for dock in docks:
+        if MAP2[dock[0]][dock[1]] == "$":
+            MAP2[dock[0]][dock[1]] = "*"
+        elif MAP2[dock[0]][dock[1]] == "@":
+            MAP2[dock[0]][dock[1]] = "+"
+        else: MAP2[dock[0]][dock[1]] = "."
     
     for m in MAP2:
         print(*m)
@@ -165,8 +177,7 @@ def printMap(state):
 def printAns(state):
     
     # count step moved
-    print("Number of steps needed: ", len(state[3]))
-    print("\n")
+    print("Number of steps needed: ", len(state[3]), "\n")
 
     #get all step moved
     steps = [direction(i) for i in state[3]]
@@ -176,7 +187,6 @@ def printAns(state):
     f = open("result.txt", "w")
     for step in steps:
         f.write(step + " ")
-    f.close()
 
 def bfs(inPut):
     global N, M, pos, boxs, goals
@@ -185,28 +195,31 @@ def bfs(inPut):
     readMap(inPut)
     printInfo()
     N, M = len(MAP), len(MAP[0])
+    print(M, N)
     init()
 
     Q = Queue() # create a queue for bfs
     state = (pos, boxs, goals, [])
     Q.put(state) # mark the first state as visited and enqueue it
     
-    ALL_STATES.append(hashState(pos[0], pos[1], state)) # next state
+    ALL_STATES.add(hashState(pos[0], pos[1], state)) # next state
 
     count = 0 # count step 
     while not Q.empty():
         # dequeue a state from queue and print it
         actState = Q.get()
         count += 1
-        print("States visited: ", count)
-        printMap(actState)
-
-        # check end game
-        if win(actState[2]):
+        
+        if win(actState[2]): # check complete
             print("\n <SOLUTION FOUND!> \n")
+            print("States visited: ", count)
+            print("\n <GOAL STATE> \n")
             printMap(actState)
             printAns(actState)
             break
+        else:
+            print("States visited: ", count, "\n")
+            printMap(actState)
         
         s_pos = actState[0]
         # get all adjacent states of current state
@@ -234,11 +247,11 @@ def gameAction():
     for step in s[0].split():
         steps.append(step)
 
-    print("\nOpen game from the link above and press ENTER to start auto solve!")
-    keyboard.wait("enter")
+    print("\nOpen game from the link above and press SPACE to start auto solve!")
+    keyboard.wait("space")
 
     # press key from list of move
-    print("\nGame solving...")
+    print("\nGAME SOLVING...")
     for move in steps:
         if move == "up":
             keyboard.press("up")
@@ -252,29 +265,65 @@ def gameAction():
         if move == "right":
             keyboard.press("right")
         
-        time.sleep(0.3)
+        time.sleep(0.25)
 
-    print("\nGame solved!")
+    print("\nGAME SOLVED!")
 
 if __name__ == "__main__":
     print("Input list:\n")
     f = open("input/listInput.txt", "r")
     print(f.read())
-
-    level = input("Choose input: ")
-    if level == "6":
-        print ("Mini Cosmos Level 6") 
+    f.close()
+    level = input("\nChoose input: ")
+    if level == "1":
+        link = "https://ksokoban.online/Mini%20Cosmos/1"
+        print ("\nMini Cosmos Level 1\n") 
+        inPut = "input/mini_1.txt" 
+    elif level == "2":
+        link = "https://ksokoban.online/Mini%20Cosmos/2"
+        print ("\nMini Cosmos Level 2\n") 
+        inPut = "input/mini_2.txt" 
+    elif level == "3":
+        link = "https://ksokoban.online/Mini%20Cosmos/3"
+        print ("\nMini Cosmos Level 3\n") 
+        inPut = "input/mini_3.txt" 
+    elif level == "4":
+        link = "https://ksokoban.online/Mini%20Cosmos/4"
+        print ("\nMini Cosmos Level 4\n") 
+        inPut = "input/mini_4.txt" 
+    elif level == "5":
+        link = "https://ksokoban.online/Mini%20Cosmos/5"
+        print ("\nMini Cosmos Level 5\n") 
+        inPut = "input/mini_5.txt"    
+    elif level == "6":
+        link = "https://ksokoban.online/Mini%20Cosmos/6"
+        print ("\nMini Cosmos Level 6\n") 
         inPut = "input/mini_6.txt"
     elif level == "7":
-        print ("Mini Cosmos Level 7") 
-        inPut = "input/mini_7.txt"
+        link = "https://ksokoban.online/Mini%20Cosmos/10"
+        print ("\nMini Cosmos Level 10\n") 
+        inPut = "input/mini_10.txt"
+    elif level == "8":
+        link = "https://ksokoban.online/Mini%20Cosmos/20"
+        print ("\nMini Cosmos Level 20\n") 
+        inPut = "input/mini_20.txt"
+    elif level == "9":
+        link = "https://ksokoban.online/Mini%20Cosmos/30"
+        print ("\nMini Cosmos Level 30\n") 
+        inPut = "input/mini_30.txt"
+    elif level == "10":
+        link = "https://ksokoban.online/Mini%20Cosmos/40"
+        print ("\nMini Cosmos Level 40\n") 
+        inPut = "input/mini_40.txt"
+    elif level == "11":
+        link = "https://ksokoban.online/Micro%20Cosmos/1"
+        print ("\nMicro Cosmos Level 1\n") 
+        inPut = "input/micro_1.txt"
 #   elif:
 
-    print("\nPress ENTER to start finding solution!")
-    keyboard.wait("enter")
-
+    startTime = time.time()
     bfs(inPut)
-    print(f.read())
-    f.close()
+    print("\nTime cost:", time.time() - startTime, "second \n")
+    print(link)
     
     gameAction()
